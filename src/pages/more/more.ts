f28d2c1ce79bcddc6ace3958d37079456ac0aa18
Salvador Alexis Gómez <salgomgon@gmail.com>
2017-10-24 12:17:14 -0500

    Primer Entrega

---
+++ src/pages/more/more.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-more',
+  templateUrl: 'more.html'
+})
+export class MorePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
