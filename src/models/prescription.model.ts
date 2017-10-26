f9acd2897f877373b7970fe5b26cd0b232120d4a
David Zarate <davidzaratetrujillo@gmail.com>
2017-10-25 20:25:00 -0500

    Add dynamic form

--- src/models/prescription.model.ts
+++ src/models/prescription.model.ts
@@ -1,6 +1,7 @@
 import { PrescriptionMedicine } from './prescription-medicine.model';
+import { DateTime } from "ionic-angular";
 
 export class Prescription {
   constructor(public id: string, public medicine: PrescriptionMedicine,
-              public frequency: Date[], public frequencyTxt: string) {}
+              public frequency: DateTime[], public frequencyTxt: string) {}
 }
--- src/pages/prescriptions/add-prescription/add-prescription.component.ts
+++ src/pages/prescriptions/add-prescription/add-prescription.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {Prescription} from '../../../models/prescription.model';
 import {PrescriptionsService} from '../../../services/prescriptions.service';
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import {OnInit} from '@angular/core';
 
 @Component({
@@ -10,13 +11,39 @@ import {OnInit} from '@angular/core';
   providers: [PrescriptionsService]
 })
 export class AddPrescriptionComponent implements OnInit {
-
+  public prescriptionForm: FormGroup; // form model
   prescription: Prescription;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private _fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.prescriptionForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(1)]],
+      activeSubstance: ['', [Validators.required, Validators.minLength(1)]],
+      dose: ['', [Validators.required, Validators.minLength(1)]],
+      units: ['', [Validators.required, Validators.minLength(1), Validators.pattern('^[0-9]*$')]],
+      treatmentTimes: this._fb.array([
+        this.initTreatmentTime()
+      ]),
+      duration: ['', [Validators.required, Validators.minLength(1), Validators.pattern('^[0-9]*$')]]
+    });
+  }
+
+  initTreatmentTime() {
+    return this._fb.group({
+      time: ['', [Validators.required]]
+    })
+  }
+
+  addTreatmentTime() {
+    const control = <FormArray>this.prescriptionForm.controls['treatmentTimes'];
+    control.push(this.initTreatmentTime());
+  }
+
+  removeTreatmentTime(i: number) {
+    const control = <FormArray>this.prescriptionForm.controls['treatmentTimes'];
+    control.removeAt(i);
   }
 
   save(model: Prescription): void {
