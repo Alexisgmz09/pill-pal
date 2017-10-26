dbd6bc0153dc239a3459467c49d33e25849f2f0d
Salvador Alexis Gómez <salgomgon@gmail.com>
2017-10-25 19:03:08 -0500

    Today Page change of style, Prescriptions obtained from services

--- src/assets/mocks/prescription.mock.ts
+++ src/assets/mocks/prescription.mock.ts
@@ -2,13 +2,13 @@ import {PrescriptionMedicine} from '../../models/prescription-medicine.model';
 import {Prescription} from '../../models/prescription.model'
 
 export let presArray: Prescription[] = [
-    new Prescription('1', new PrescriptionMedicine('1a', 'Lozartan', '', '50 mg', 10), 1,2, '9:00 am, 9:00 pm', 5),
-    new Prescription('2', new PrescriptionMedicine('2a', 'Ibuprofeno', '', '200 mg', 5), 1, 2, '7:00 am, 1:00 pm, 10:00 pm', 5),
-    new Prescription('3', new PrescriptionMedicine('3a', 'Paracetamol', '', '30 mg', 1), 1, 2, '8:00 am, 8:00 pm', 5),
-    new Prescription('4', new PrescriptionMedicine('4a', 'Omeprazol', '', '600 mg', 3), 1, 2, '2:00 pm', 5)
+    new Prescription('1', new PrescriptionMedicine('1a', 'Lozartan', '', '50 mg', 10),null, '9:00 am, 9:00 pm'),
+    new Prescription('2', new PrescriptionMedicine('2a', 'Ibuprofeno', '', '200 mg', 5),null, '7:00 am, 1:00 pm, 10:00 pm'),
+    new Prescription('3', new PrescriptionMedicine('3a', 'Paracetamol', '', '30 mg', 1), null, '8:00 am, 8:00 pm'),
+    new Prescription('4', new PrescriptionMedicine('4a', 'Omeprazol', '', '600 mg', 3), null, '2:00 pm')
 ];
 export let currentPresArray: Prescription[] =[
-    new Prescription('1', new PrescriptionMedicine('1a', 'Lozartan', '', '50 mg', 10), 1,2, '9:00 pm', 5),
-    new Prescription('2', new PrescriptionMedicine('2a', 'Ibuprofeno', '', '200 mg', 5), 1, 2, ' 10:00 pm', 5),
-    new Prescription('3', new PrescriptionMedicine('3a', 'Paracetamol', '', '30 mg', 1), 1, 2, ' 8:00 pm', 5)
+    new Prescription('1', new PrescriptionMedicine('1a', 'Lozartan', '', '50 mg', 10), null, '9:00 pm'),
+    new Prescription('2', new PrescriptionMedicine('2a', 'Ibuprofeno', '', '200 mg', 5), null, ' 10:00 pm'),
+    new Prescription('3', new PrescriptionMedicine('3a', 'Paracetamol', '', '30 mg', 1), null, ' 8:00 pm')
 ];
--- src/services/prescriptions.service.ts
+++ src/services/prescriptions.service.ts
@@ -7,11 +7,18 @@ import { presArray, currentPresArray } from '../assets/mocks/prescription.mock';
 export class PrescriptionsService {
 
   prescriptionsArray: Prescription[];
+  currentPrescriptions: Prescription[];
 
   getPrescriptions(): Promise<Prescription[]> {
     if(this.prescriptionsArray === undefined){
       this.prescriptionsArray=presArray;
     }
     return Promise.resolve(this.prescriptionsArray);
+  }
+  getCurrentPrescriptions(): Promise<Prescription[]>{
+    if(this.currentPrescriptions === undefined){
+      this.currentPrescriptions = currentPresArray;
+    }
+    return Promise.resolve(this.currentPrescriptions);
   }
 }
