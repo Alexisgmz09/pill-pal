730d7610c09da3f0cb36c746c19bb549c0cf07b6
Salvador Alexis Gómez <salgomgon@gmail.com>
2017-11-25 15:32:05 -0600

    Ya funciona Open pay

--- src/pages/more/cards/card/card.ts
+++ src/pages/more/cards/card/card.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Validators, FormGroup, FormArray, FormBuilder  } from '@angular/forms';
+import { OnInit } from '@angular/core';
+
+declare var jQuery:any;
+declare var OpenPay:any;
 
 @Component({
   selector: 'page-card',
   templateUrl: 'card.html'
 })
-export class CardPage {
+export class CardPage implements OnInit {
    public prescriptionForm: FormGroup;
-
+    public deviceSessionId;
   constructor(public navCtrl: NavController,private _fb: FormBuilder,) {
+    console.log(OpenPay);
+    OpenPay.setId('m6l9ig4gbsmqaxftlujo');
+    OpenPay.setApiKey('pk_34b1d91350b64727a8440feabb6efd6e');
+    OpenPay.setSandboxMode(true);
+    this.deviceSessionId = OpenPay.deviceData.setup()
     this.prescriptionForm = this._fb.group({
         name: ['', [Validators.required, Validators.minLength(1)]],
         number: ['', [Validators.required, Validators.minLength(1)]],
@@ -19,5 +28,7 @@ export class CardPage {
       });
   }
 
-  
+  ngOnInit(){
+    
+  }
 }
